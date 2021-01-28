77735c8c1413d541e1f374f9a2466f2b4f473ce8
gohin <gohin.0626@gmail.com>
2021-01-28 17:15:35 +0800

    reflect

---
+++ src/main/java/com/kure/test/thread/CountDownLatchTest.java
@@ -0,0 +1,51 @@
+package com.kure.test.thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchTest {
+
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(3);
+    public static void main(String[] args) throws InterruptedException {
+        ArrayList list = new ArrayList();
+        for (int i =0; i< 10000; i++) {
+            list.add(i);
+        }
+
+        int index = 0;
+        CountDownLatch countDownLatch;
+        for(int i= 0; i< 3; i++) {
+            int nextIdx = Math.min(index + 1000, list.size());
+            List a = list.subList(index, nextIdx);
+            countDownLatch = new CountDownLatch(1);
+            executorService.submit(new MyThread(a,countDownLatch));
+            countDownLatch.await();
+            index = nextIdx;
+        }
+        executorService.shutdown();
+    }
+}
+
+class MyThread implements Runnable{
+
+    private List execList;
+    private CountDownLatch countDownLatch;
+
+    public MyThread(List list, CountDownLatch countDownLatch) {
+        this.execList = list;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            execList.forEach(System.out::println);
+        }finally {
+            countDownLatch.countDown();
+        }
+
+    }
+}
