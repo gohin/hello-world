57ee802721a10924d2e90fb1d2c2dbdb4f749369
gohin <gohin.0626@gmail.com>
2021-03-09 10:27:14 +0800

    reflect

--- src/main/java/com/kure/test/spring/spel/SpelTest.java
+++ src/main/java/com/kure/test/spring/spel/SpelTest.java
@@ -6,12 +6,26 @@ import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SpelTest {
 
     public static void main(String[] args) {
         Inventor inventor = new Inventor("kure", new Date(), "626");
+
+        Inventor kure1 = new Inventor("kure1", new Date(), "626");
+        Inventor kure2 = new Inventor("kure2", new Date(), "626");
+        Inventor kure3 = new Inventor("kure3", new Date(), "626");
+        List<Inventor> inventors = Arrays.asList(kure1, kure2, kure3);
+
+        Map<String, Inventor> map = new HashMap<>();
+        map.put("kure1", kure1);
+        map.put("kure2", kure2);
+        map.put("kure3", kure3);
         StandardEvaluationContext context = new StandardEvaluationContext();
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
         beanFactory.registerSingleton("inventor1", inventor);
@@ -19,10 +33,19 @@ public class SpelTest {
 
         context.setVariable("getInt", SpelTest.class.getMethods()[1]);
         context.setVariable("inventor", new Inventor("kure", new Date(), "626"));
+        context.setVariable("inventors", inventors);
+        context.setVariable("maps", map);
+
         ExpressionParser parser = new SpelExpressionParser();
 
         // 获取bean
         System.out.println(parser.parseExpression("@inventor1.name").getValue(context));
+
+        // list
+        System.out.println("list=" + parser.parseExpression("#inventors[2].name").getValue(context));
+
+        // map
+        System.out.println("map=" + parser.parseExpression("#maps['kure1'].name").getValue(context));
 
         System.out.println(parser.parseExpression("5 == 4").getValue());
         System.out.println(parser.parseExpression("5 eq 5").getValue());
