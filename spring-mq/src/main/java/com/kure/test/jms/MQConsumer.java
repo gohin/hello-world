1572b64c5f73ae91c0ccdcf8cb54a182a5165e98
gohin <gohin.0626@gmail.com>
2021-03-21 21:41:50 +0800

    rocketMQ

--- spring-mq/src/main/java/com/kure/test/jms/MQConsumer.java
+++ spring-mq/src/main/java/com/kure/test/jms/MQConsumer.java
@@ -22,14 +22,9 @@ public class MQConsumer {
         // Subscribe one more more topics to consume.
         consumer.subscribe("TopicTest", "*");
         // Register callback to execute on arrival of messages fetched from brokers.
-        consumer.registerMessageListener(new MessageListenerConcurrently() {
-
-            @Override
-            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
-                                                            ConsumeConcurrentlyContext context) {
-                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
-                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-            }
+        consumer.registerMessageListener((MessageListenerConcurrently) (msgs, context) -> {
+            System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
 
         //Launch the consumer instance.
