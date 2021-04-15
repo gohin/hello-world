6dddb89ceaf0536f31c7cc74acb11539f51b6064
gohin <gohin.0626@gmail.com>
2021-04-15 21:08:55 +0800

    rocketMQ

--- test-main/src/main/java/com/kure/test/base/ClassLoaderT.java
+++ test-main/src/main/java/com/kure/test/base/ClassLoaderT.java
@@ -8,16 +8,23 @@ package com.kure.test.base;
  */
 public class ClassLoaderT {
 
+    private static int a = 1; // 类变量 线程不安全
+
+    private char c = 11; // 成员变量 线程不安全
+
     public static void main(String[] args) {
+        String s = "111"; // 局部变量 args 局部变量 线程安全
         System.out.println(T.count);
     }
 }
 class T{
 //    public static T t = new T();  // 2
-    public static int count = 2;
-    public static T t = new T(); // 3
 
-    // M->0 M->8
+    public static int count = 2;
+    public static T t = new T();   // 3
+
+    // M->0 M->
+
     private int m = 8;
 
     private T(){
