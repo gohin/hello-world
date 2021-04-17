9c815f1427c5c5d537c9219ef8612eea1ecefd1b
gohin <gohin.0626@gmail.com>
2021-04-17 21:04:08 +0800

    rocketMQ

---
+++ test-main/src/main/java/com/kure/test/base/HashMapTest.java
@@ -0,0 +1,13 @@
+package com.kure.test.base;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        // 链表转树 阈值8
+        // 树转链表 阈值6
+        // 默认长度为什么是16
+        // 扩容因子为什么是0.75
+        // hashMap 长度2的幂 低位掩码 与运算后 高位都是0 散列分布 避免hash冲突
+
+        // 为什么使用红黑树 提高效率
+    }
+}
